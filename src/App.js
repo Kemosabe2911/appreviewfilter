bd00781f60bf2d7e25dfb2495bf78cf31313afe3
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-05 16:56:25 +0530

    Update app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import review from './data/review.json';
 import './App.css';
 
 function App() {
-  //console.log(review);
+  //console.log(review[0].appID);
+  console.log(review);
+  const [result,setResult] = useState([]);
+
   return (
       <div className="container">
 
