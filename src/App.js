7fe553e9e547b691f2bab80c5e4bac80a963bf37
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-05 16:46:00 +0530

    Add review json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import review from './data/review.json';
 import './App.css';
 
 function App() {
+  //console.log(review);
   return (
       <div className="container">
-        
+
       </div>
   );
 }
