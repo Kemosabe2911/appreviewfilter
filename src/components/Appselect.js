63eb806b89c615c8141df03e8469915dba9ab6a8
Kemosabe2911 <stevinprince29@gmail.com>
2020-11-09 16:50:01 +0530

    Update css

--- src/components/Appselect.js
+++ src/components/Appselect.js
@@ -18,17 +18,22 @@ const Appselect = ({ apps, appselected, timeorder, handleAppselected, handleTime
       */
     //console.log(appIDs);
     return (
+        
         <form>
+            <div className="selectdiv">
             <select className="app-select" defaultValue={appselected} onChange={handleAppselected} placeholder={<div>Select An App</div>}>
                 <option disabled={false} value="">-</option>
                 {uniqueappIDs.map(app =>(
                     <option key={app} value={app}>{app}</option>
                 ))}
             </select>
+            </div>
+            <div className="selectdiv2">
             <select className="time-select" defaultValue={timeorder} onChange={handleTimeorder} >
                 <option value="newest">newest</option>
                 <option value="oldest">oldest</option>
             </select>
+            </div>
         </form>
     )
 }
